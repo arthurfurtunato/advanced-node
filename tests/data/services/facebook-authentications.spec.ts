d48ef4da48ac4102136c968fc3a0290edcde8466
arthurfurtunato <arthurfurtunato@gmail.com>
2022-08-16 13:22:11 -0300

    ensure Service returns AuthenticationError when LoadFacebookUserApi returns undefined

--- tests/data/services/facebook-authentications.spec.ts
+++ tests/data/services/facebook-authentications.spec.ts
@@ -1,27 +1,33 @@
+import { AuthentiocationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
 class FacebookAuthenticationService {
   constructor (private readonly loadFacebookUserApi: LoadFacebookUser) {}
-  async perform (params: FacebookAuthentication.Params): Promise<void> {
+  async perform (params: FacebookAuthentication.Params): Promise<AuthentiocationError> {
     await this.loadFacebookUserApi.loadUser(params)
+    return new AuthentiocationError()
   }
 }
 
 interface LoadFacebookUser {
-  loadUser: (params: LoadFacebookUser.Params) => Promise<void>
+  loadUser: (params: LoadFacebookUser.Params) => Promise<LoadFacebookUser.Result>
 }
 
 namespace LoadFacebookUser {
   export type Params = {
     token: string
   }
+
+  export type Result = undefined
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUser {
   token?: string
+  result = undefined
 
-  async loadUser (params: LoadFacebookUser.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUser.Params): Promise<LoadFacebookUser.Result> {
     this.token = params.token
+    return this.result
   }
 }
 
@@ -33,5 +39,15 @@ describe('FacebookAuthentications', () => {
     await sut.perform({ token: 'any_token' })
 
     expect(loadFacebookUserApi.token).toBe('any_token')
+  })
+
+  it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
+    const loadFacebookUserApi = new LoadFacebookUserApiSpy()
+    loadFacebookUserApi.result = undefined
+    const sut = new FacebookAuthenticationService(loadFacebookUserApi)
+
+    const authResult = await sut.perform({ token: 'any_token' })
+
+    expect(authResult).toEqual(new AuthentiocationError())
   })
 })
